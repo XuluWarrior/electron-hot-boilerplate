d322131b7b20078e609385f61e373941370101ff
geowarin <geowarin@gmail.com>
2016-03-04 23:25:27 +0100

    Watch

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,6 @@ const React = require('react');
 
 module.exports = class App extends React.Component {
   render() {
-    return <h1>HOP</h1>
+    return <h1>HOP lol</h1>
   }
 };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ require('jsx-hook')({harmony: true});
 const React = require('react');
 const ReactDOM = require('react-dom');
 const App = require('./src/App.jsx');
-const App2 = require('./src/App2.jsx');
+
+const watch = require('watch');
 
 const createProxy = require('./src/proxy/index');
 const deepForceUpdate = require('react-deep-force-update');
@@ -13,7 +14,13 @@ const Proxy = proxy.get();
 
 var rootInstance = ReactDOM.render(React.createElement(Proxy), document.getElementById('root'));
 
-setTimeout(() => {
-    proxy.update(App2);
-    deepForceUpdate(rootInstance);
-}, 1000);
+
+watch.createMonitor('./src', function (monitor) {
+    monitor.on("changed", function (f, curr, prev) {
+        console.log(f);
+        delete require.cache[require.resolve('./' + f)];
+        var newCompo = require('./' + f);
+        proxy.update(newCompo);
+        deepForceUpdate(rootInstance);
+    });
+});
