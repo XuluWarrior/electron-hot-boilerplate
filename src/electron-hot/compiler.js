0b865a631ce609fc358d56ebe72edaa2bfed69ec
geowarin <geowarin@gmail.com>
2016-03-05 16:14:45 +0100

    Add a mocha test with enzyme

--- src/electron-hot/compiler.js
+++ src/electron-hot/compiler.js
@@ -2,4 +2,4 @@
 // Can be used as a mocha compiler
 // mocha --compilers electron-hot/compiler.js
 // This will not instrument the files and hot reload will be disabled
-require('./electron-hot/jsxTransform').install({doNotInstrument: true});
+require('./jsxTransform').install({doNotInstrument: true});
