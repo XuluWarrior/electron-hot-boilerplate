1a3630c32cefd6971612433979c659ba3b5b29a3
geowarin <geowarin@gmail.com>
2016-03-05 15:58:56 +0100

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-require('./electron-hot/jsxTransform').install();
-
-const electronHot = require('./electron-hot');
+if (process.env.NODE_ENV === 'development') {
+    require('./electron-hot/jsxTransform').install();
+    const electronHot = require('./electron-hot');
+    electronHot.watchJsx(['src/**/*.jsx']);
+    electronHot.watchCss(['assets/**/*.css']);
+} else {
+    require('./electron-hot/jsxTransform').install({doNotInstrument: true});
+}
 
 require('./index.jsx');
-
-electronHot.watchJsx(['src/**/*.jsx']);
-electronHot.watchCss(['assets/**/*.css']);
