e132410465282c8c3d1d4c29c1766b209b2010bd
geowarin <geowarin@gmail.com>
2016-03-05 09:40:07 +0100

    Use watch glob

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ require('jsx-hook')({harmony: true});
 
 const React = require('react');
 const ReactDOM = require('react-dom');
-const App = require('./src/App.jsx');
+const App = require('./App.jsx');
 
-const watch = require('watch');
+const watchGlob = require('watch-glob');
 
-const createProxy = require('./src/proxy/index');
+const createProxy = require('./proxy/index');
 const deepForceUpdate = require('react-deep-force-update');
 
 const proxy = createProxy.default(App);
@@ -14,27 +14,22 @@ const Proxy = proxy.get();
 
 var rootInstance = ReactDOM.render(React.createElement(Proxy), document.getElementById('root'));
 
-
-watch.createMonitor('./src', function (monitor) {
-    monitor.on("changed", function (f, curr, prev) {
-        console.log(f);
-        delete require.cache[require.resolve('./' + f)];
-        var newCompo = require('./' + f);
-        proxy.update(newCompo);
-        deepForceUpdate(rootInstance);
-    });
+watchGlob(['src/**/*.jsx'], {callbackArg: 'absolute'}, f => {
+    console.log(f);
+    delete require.cache[require.resolve(f)];
+    var newCompo = require(f);
+    proxy.update(newCompo);
+    deepForceUpdate(rootInstance);
 });
 
-watch.createMonitor('./assets', function (monitor) {
-    monitor.on("changed", function (f, curr, prev) {
-        console.log(f);
-        var links = document.getElementsByTagName("link");
-        for (var i = 0; i < links.length; i++) {
-            var link = links[i];
+watchGlob(['assets/**/*.css'], {callbackArg: 'absolute'}, f => {
+    console.log(f);
+    var links = document.getElementsByTagName('link');
+    for (var i = 0; i < links.length; i++) {
+        var link = links[i];
 
-            if (link.href.indexOf("css") > -1) {
-                link.href = link.href + "?id=" + new Date().getMilliseconds();
-            }
+        if (link.href.indexOf('css') > -1) {
+            link.href = link.href + '?id=' + new Date().getMilliseconds();
         }
-    });
+    }
 });
