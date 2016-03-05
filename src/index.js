cdf147729e00fbb2d8a70b0fad3885ccb560b842
geowarin <geowarin@gmail.com>
2016-03-05 09:48:00 +0100

    Extract hot css

--- src/index.js
+++ src/index.js
@@ -2,19 +2,7 @@ require('jsx-hook')({harmony: true});
 
 const App = require('./App.jsx');
 const hotRender = require('./electron-hot/hotRender');
-
-const watchGlob = require('watch-glob');
+const hotCss = require('./electron-hot/hotCss');
 
 hotRender(App, document.getElementById('root'));
-
-watchGlob(['assets/**/*.css'], {callbackArg: 'absolute'}, f => {
-    console.log(f);
-    var links = document.getElementsByTagName('link');
-    for (var i = 0; i < links.length; i++) {
-        var link = links[i];
-
-        if (link.href.indexOf('css') > -1) {
-            link.href = link.href + '?id=' + new Date().getMilliseconds();
-        }
-    }
-});
+hotCss();
