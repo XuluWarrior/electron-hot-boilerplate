32f1b5db73949e64cf3f5f2e9b0f427ec41ce527
geowarin <geowarin@gmail.com>
2016-03-11 11:52:29 +0100

    Hot reload reducers

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const App = require('./ui/containers/App.jsx');
+const watchGlob = require('watch-glob');
 
 const Provider = require('react-redux').Provider;
 const createStore = require('redux').createStore;
@@ -8,6 +9,13 @@ const createStore = require('redux').createStore;
 const counter = require('./reducer/counter');
 
 const store = createStore(counter);
+
+watchGlob(['src/reducer/**/*.js'], {callbackArg: 'absolute'}, f => {
+    console.log('Hot reload reducer', f);
+    delete require.cache[require.resolve(f)];
+    const nextReducer = require(f);
+    store.replaceReducer(nextReducer);
+});
 
 ReactDOM.render(<Provider store={store}>
     <App />
